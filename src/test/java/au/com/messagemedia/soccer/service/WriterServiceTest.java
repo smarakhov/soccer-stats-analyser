526e66639c443e69587fe0c7e8d2e943214c09e2
Sergey Marakhov <sergey.marakhov@myob.com>
2018-09-09 14:47:45 +1000

    SSA-7: Final cleanup

--- src/test/java/au/com/messagemedia/soccer/service/WriterServiceTest.java
+++ src/test/java/au/com/messagemedia/soccer/service/WriterServiceTest.java
@@ -1,17 +1,27 @@
 package au.com.messagemedia.soccer.service;
 
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class WriterServiceTest {
 
+  private static final String TEST_OUTPUT_FILENAME = "test-output.csv";
+
   private WriterService writerService;
+
+  @AfterClass
+  public static void afterClass() {
+    new File(TEST_OUTPUT_FILENAME).deleteOnExit();
+  }
 
   @Before
   public void setup() {
@@ -21,10 +31,11 @@ public class WriterServiceTest {
   @Test
   public void testGetWriterWithFilename() throws FileNotFoundException {
     // when
-    PrintWriter writer = writerService.getWriter("output.cvs");
+    PrintWriter writer = writerService.getWriter(TEST_OUTPUT_FILENAME);
 
     // then
     assertThat(writer, notNullValue());
+    assertThat(new File(TEST_OUTPUT_FILENAME).exists(), is(true));
   }
 
   @Test(expected = FileNotFoundException.class)
